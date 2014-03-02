7b2fa99da91b2c6d896c899d2ee1311674457712
HIGAKI Masaru <mas.higa@gmail.com>
2014-03-02 20:19:48 +0900

    question

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+#! /usr/bin/env ruby
+
+a = [1, 2, 3, 5]
+
+# a.select{|i| i.odd?}            # => [1, 3, 5]
---
+++ ex3.rb
@@ -0,0 +1,5 @@
+#! /usr/bin/env ruby
+
+a = [1, 2, 3, 5]
+
+# a.inject{|s, i| s + i}          # => 11
