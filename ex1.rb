fea3f9086549c59f20f2e1c22730fd691c9ee6bd
HIGAKI Masaru <mas.higa@gmail.com>
2014-03-23 14:28:10 +0900

    joke :)

--- ex1.rb
+++ ex1.rb
@@ -4,4 +4,14 @@ a = [1, 2, 3, 5]
 
 # a.map{|i| i * i}                # => [1, 4, 9, 25]
 
-a.map!{|i| i * i}               # => [1, 4, 9, 25]
+class Array
+  def __map__
+    if block_given?
+      inject([]){|r, i| r << yield(*i)}
+    else
+      to_enum :__map__
+    end
+  end
+end
+
+a.__map__{|i| i * i}            # => [1, 4, 9, 25]
