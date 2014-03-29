d927db883556ced19a7e7bbfb7241dcdfdf0d40b
HIGAKI Masaru <mas.higa@gmail.com>
2014-03-29 21:42:32 +0900

    each_with_object

--- ex4-1.rb
+++ ex4-1.rb
@@ -9,7 +9,7 @@ end
 "No Ruby, No Life".words        # => ["No", "Ruby", "No", "Life"]
 
 puts "No Ruby, No Life.".words
-  .inject(Hash.new{|h, k| h[k] = 0}){|h, k| h[k] += 1; h}
+  .each_with_object(Hash.new{|h, k| h[k] = 0}){|k, h| h[k] += 1; h}
   .sort_by{|k, v| [-v, k]}
   .map{|k, v| "%8d: %s" % [v, k]}
 
